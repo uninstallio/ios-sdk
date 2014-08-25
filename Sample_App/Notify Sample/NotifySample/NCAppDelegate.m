b7fa97f68d00a132593f487207fca0b7b53042b0
Alok Mishra <alok@notiphi.com>
2014-08-25 20:05:23 +0530

    Sample code changes in app delegate

--- Sample_App/Notify Sample/NotifySample/NCAppDelegate.m
+++ Sample_App/Notify Sample/NotifySample/NCAppDelegate.m
@@ -34,22 +34,49 @@
     return YES;
 }
 
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    //Starts Notify Services
+    NSLog(@"Going to background");
+
+}
+
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     //Stops notify services
-    //NSLog(@"Starting notiphi services");
+    NSLog(@"Starting notiphi services");
     [[NotifyManager sharedManager] startNotifyServicesWithAppID:@"demopro" key:@"PNNe5wL2ANnD6pVUysJk"];
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 
 -(void) application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken{
-    //NSLog(@"Registerd for push with device token: %@", deviceToken);
+    NSLog(@"Registerd for push with device token: %@",deviceToken);
     [[NotifyManager sharedManager] registerForPushNotificationUsingDeviceToken:deviceToken];
 }
 
 - (void) application:(UIApplication *)application performFetchWithCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
 {
+    NSLog(@"Inside timed background fetch \n");
     [[NotifyManager sharedManager] startNotifyServicesWithAppID:@"demopro" key:@"PNNe5wL2ANnD6pVUysJk"];
     completionHandler(UIBackgroundFetchResultNoData);
 }
